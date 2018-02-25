d42b8fc22cf7ce821232d82b781bb57d9b5a6454
Nakul <nakulyadlapati@gmail.com>
2018-02-25 06:39:57 -0800

    Added two satements after reverting

--- Java_test/src/com/icici/loans/carloans/TestGit.java
+++ Java_test/src/com/icici/loans/carloans/TestGit.java
@@ -4,6 +4,9 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Added a Statement and Going to push");
+		System.out.println("Add another statement");
 
 	}
 
