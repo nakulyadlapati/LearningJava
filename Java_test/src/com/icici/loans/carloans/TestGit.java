ed77bc27ee1775f32e99d4c5637d6fb7733f8e40
Nakul <nakulyadlapati@gmail.com>
2018-02-25 06:46:00 -0800

    Reverted the two statements

--- Java_test/src/com/icici/loans/carloans/TestGit.java
+++ Java_test/src/com/icici/loans/carloans/TestGit.java
@@ -3,7 +3,7 @@ package com.icici.loans.carloans;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub1
 
 	}
 
